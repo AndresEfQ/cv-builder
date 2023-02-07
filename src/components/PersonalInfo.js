608ea56ee7f0c76bee9731a6a67fcc08f494b16c
AndresEfQ <andresf2641@gmail.com>
2023-02-07 14:35:47 -0500

    Extract form styles to a different file

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,46 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import "@fontsource/raleway/700.css";
-
-const StyledFieldset = styled.fieldset`
-  border-radius: 5px;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-const StyledForm = styled.form`
-  font-family: "Raleway", Arial, Helvetica, sans-serif;
-  width: 100%;
-  & textarea {
-    resize: none;
-  }
-`;
-
-const StyledUl = styled.ul`
-  list-style: none;
-
-  & li {
-    margin: 0.5em;
-    display: flex;
-    flex-direction: column;
-
-    & input {
-      height: 1.5em;
-      border-radius: 5px;
-      border: 1px solid rgb(200, 200, 200);
-      outline: none;
-    }
-  }
-`;
+import StyledForm from "./StyledForm";
 
 class PersonalInfo extends React.Component {
   render () {
     return (
       <StyledForm>
-        <StyledFieldset>
+        <fieldset>
           <legend>Personal Information</legend>
-          <StyledUl>
+          <ul>
             <li>
               <label htmlFor="firstName">First Name</label>
               <input type="text" name="firstName" id="firstName" />
@@ -73,8 +42,8 @@ class PersonalInfo extends React.Component {
               <label htmlFor="description">Description</label>
               <textarea name="description" id="description" cols="30" rows="3" placeholder="A short description of who are you and your skills" onResize={false}></textarea>
             </li>
-          </StyledUl>
-        </StyledFieldset>
+          </ul>
+        </fieldset>
       </StyledForm>
     )
   }
