3dc561d62f68fc9f3177e39a722835e0463ab2e7
AndresEfQ <andresf2641@gmail.com>
2023-02-07 21:15:24 -0500

    Create styled button

---
+++ src/components/StyledButton.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 12px 18px;
+  border-radius: 5px;
+  border: none;
+  background-color: rgb(180, 0, 0);
+  font-size: 16px;
+  font-weight: bold;
+  color: rgb(255, 255, 255);
+`;
+
+export default StyledButton;
